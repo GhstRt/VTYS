859087c60345199e00dca9fe17f65ac9e0bfebe8
Mustafa KARATAŞ <91211621+MUSTAFAKARATAS0@users.noreply.github.com>
2024-01-07 14:31:44 +0300

    .

--- frontend/src/components/pages/Seyahat.js
+++ frontend/src/components/pages/Seyahat.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import './PlaceTable.css'; // Stil dosyasını ekleyin
+import './PlaceTable.css'; 
 
 const PlaceTable = () => {
   const [places, setPlaces] = useState([]);
