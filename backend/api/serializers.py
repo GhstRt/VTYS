5d36fd4cb012d4837eba701bc748cf92612d0f86
Mustafa KARATAŞ <91211621+MUSTAFAKARATAS0@users.noreply.github.com>
2024-07-11 13:07:39 +0300

    .

--- backend/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Ulke
-
-
-class UlkeSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Ulke
-        fields = ['Name', 'Type', 'Location', 'Latitude', 'Longitude', 'pictureLink', 'wikipediaLink', 'buildInYear']
--- backend/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('places/<str:id>/', LocationView.as_view()),
-    path('places/', LocationView.as_view()),
-    path('chat/', ChatAPIView.as_view())
-]
